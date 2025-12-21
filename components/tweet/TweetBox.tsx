11e965517df096ea51a69d6f9e6e2ebc73487c23
Fabe Z <zengjiayifabe@outlook.com>
2025-12-20 23:24:54 -0500

    edit tweet box textarea

--- components/tweet/TweetBox.tsx
+++ components/tweet/TweetBox.tsx
@@ -1,24 +1,32 @@
+'use client';
 import { CgProfile } from "react-icons/cg";
 import styles from '../../styles/TweetBox.module.css';
 import { FaImage } from "react-icons/fa";
 import { MdEmojiEmotions } from "react-icons/md";
 import { HiGif } from "react-icons/hi2";
+import { useState } from "react";
 export default function  TweetBox() {
+  const [text, setText] = useState('');
   return (
     <div className={styles.tweetBox}>
       <CgProfile className={styles.profileIcon} title='Profile Icon' />
       <div className={styles.divider}>
-        <textarea className={styles.textarea} placeholder="What's happening?" />
+        <textarea 
+          className={styles.textarea} 
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="What's happening?"
+          rows={3} />
         <div className={styles.clickableArea}>
           <ul className={styles.actionList}>
             <li className={styles.actionItem}>
-              <FaImage title="image"/>
+              <FaImage className={styles.icon} title="image"/>
             </li>
             <li className={styles.actionItem}>
-              <HiGif title="gif"/>
+              <HiGif className={styles.icon} title="gif"/>
             </li>
             <li className={styles.actionItem}>
-              <MdEmojiEmotions title="emoji"/>
+              <MdEmojiEmotions className={styles.icon} title="emoji"/>
             </li>
           </ul>
           <button className={styles.tweetButton}>Tweet</button>
