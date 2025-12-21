627001d75c131820fd65f4d983ae33815d8726de
Fabe Z <zengjiayifabe@outlook.com>
2025-12-20 20:32:48 -0500

    added header layout

---
+++ components/layout/Header.tsx
@@ -0,0 +1,34 @@
+import styles from '../../styles/Header.module.css';
+import { FaHome, FaSearch} from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { FaXTwitter } from "react-icons/fa6";
+export default function Header({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <header>
+        <nav className={styles.nav}>
+            <ul className={styles.navList}>
+                <li>
+                    <FaXTwitter className={styles.icons} title='Logo'/> 
+                </li>
+                <li>
+                    <FaHome className={styles.icons} title='Home'/>
+                    <p className={styles.navItem}>Home</p>
+                </li>
+                <li>
+                    <FaSearch className={styles.icons} title='Explore'/>
+                    <p className={styles.navItem}>Explore</p>  
+                </li>
+                <li>
+                    <CgProfile className={`${styles.icons} ${styles.profileIcon}`} title='Profile'/>
+                    <p className={styles.navItem}>Profile</p>
+                </li>
+            </ul>
+            {children}
+        </nav>
+    </header>
+  );    
+}
