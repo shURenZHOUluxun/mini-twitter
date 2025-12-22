a1c11fee2a522d457d509b946eebbb38fb0a27f7
Fabe Z <zengjiayifabe@outlook.com>
2025-12-22 17:31:41 -0500

    new feature tweetcard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/layout/Header";
 import TweetBox from "@/components/tweet/TweetBox";
+import TweetCard from "@/components/tweet/TweetCard";
 
 export default function Home() {
     return (
         <>
             <TweetBox />
+            <TweetCard />
         </>
     );
 }
---
+++ components/tweet/TweetCard.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/TweetCard.module.css";
+
+export default function TweetCard() {
+  return (
+    <section className={styles.cardContainer}>
+      Tweet Card Component
+    </section>
+  );
+}            
