7a3b290c3d5ea95f4d004a52d20c80288e3daed5
Fabe Z <zengjiayifabe@outlook.com>
2025-12-22 17:49:03 -0500

    edit added demo tweet content

--- components/tweet/TweetCard.tsx
+++ components/tweet/TweetCard.tsx
@@ -1,9 +1,21 @@
 import styles from "@/styles/TweetCard.module.css";
-
+import { CgProfile } from "react-icons/cg";
+import Image from "next/image";
 export default function TweetCard() {
   return (
     <section className={styles.cardContainer}>
-      Tweet Card Component
+        <CgProfile className={styles.profileIcon} title='Profile Icon' />
+        
+        <div className={styles.tweetContent}>
+            <div className={styles.tweetHeader}>
+                <span className={styles.name}>Nike name</span>
+                <span className={styles.username}>@username</span>
+                <span className={styles.dot}>·</span>
+                <span className={styles.time}>3h</span>
+            </div>
+            <p className={styles.tweetText}>This is a sample tweet content to demonstrate the TweetCard component.</p>
+            <Image src="/uoft_grass.webp" alt="Tweet Image" width={500} height={300} className={styles.tweetImage} />
+        </div>
     </section>
   );
 }            
