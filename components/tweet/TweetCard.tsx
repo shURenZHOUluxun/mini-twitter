8683f5ef4cc2e71f35fb747c413bdb7304790cb4
Fabe Z <zengjiayifabe@outlook.com>
2025-12-26 04:39:17 -0500

    added comment icon

--- components/tweet/TweetCard.tsx
+++ components/tweet/TweetCard.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/TweetCard.module.css";
 import { CgProfile } from "react-icons/cg";
 import { AiOutlineLike } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
 import Image from "next/image";
 export default function TweetCard() {
   return (
@@ -9,14 +10,17 @@ export default function TweetCard() {
         
         <div className={styles.tweetContent}>
             <div className={styles.tweetHeader}>
-                <span className={styles.name}>Nike name</span>
-                <span className={styles.username}>@username</span>
-                <span className={styles.dot}>·</span>
-                <span className={styles.time}>3h</span>
+              <span className={styles.name}>Nike name</span>
+              <span className={styles.username}>@username</span>
+              <span className={styles.dot}>·</span>
+              <span className={styles.time}>3h</span>
             </div>
             <p className={styles.tweetText}>This is a sample tweet content to demonstrate the TweetCard component.</p>
             <Image src="/uoft_grass.webp" alt="Tweet Image" width={500} height={300} className={styles.tweetImage} />
-            <AiOutlineLike className={styles.likeIcon} title='Like Icon' />
+            <div className={styles.tweetActions}>
+              <AiOutlineLike className={styles.likeIcon} title='Like' />
+              <FaRegComment className={styles.commentIcon} title='Comment' />
+            </div>
         </div>
     </section>
   );
