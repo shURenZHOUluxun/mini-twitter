15906cc88c12d8230f917ecf2355d35ef5f0a987
Fabe Z <zengjiayifabe@outlook.com>
2025-12-31 20:43:11 -0500

    created profile style css

--- components/tweet/Profile.tsx
+++ components/tweet/Profile.tsx
@@ -1,5 +1,10 @@
+import { CgProfile } from "react-icons/cg";
+import styles from '../../styles/Profile.module.css';
 export default function Profile() {
   return (
-    <div>Profile Component</div>
+    <div>
+       <CgProfile className={styles.profileIcon} title='Profile Icon' /> 
+    </div>
+    
   );
 }
