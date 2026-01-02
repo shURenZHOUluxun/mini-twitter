ffc44c0dbe947c610923ddf0f81cff0e3a7b864a
Fabe Z <zengjiayifabe@outlook.com>
2026-01-01 23:54:52 -0500

    added username

--- components/tweet/Profile.tsx
+++ components/tweet/Profile.tsx
@@ -4,6 +4,8 @@ export default function Profile() {
   return (
     <div>
        <CgProfile className={styles.profileIcon} title='Profile Icon' /> 
+       <h1 className={styles.name}>User Name</h1>
+       <p className={styles.username}>@username</p>
     </div>
     
   );
