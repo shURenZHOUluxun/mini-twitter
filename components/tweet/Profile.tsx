8fac0c9f08909daffa0b689c53dbb18b084d3842
Fabe Z <zengjiayifabe@outlook.com>
2026-01-04 23:53:15 -0500

    edit profile

--- components/tweet/Profile.tsx
+++ components/tweet/Profile.tsx
@@ -1,11 +1,18 @@
 import { CgProfile } from "react-icons/cg";
 import styles from '../../styles/Profile.module.css';
+import Image from 'next/image';
 export default function Profile() {
   return (
-    <div>
-       <CgProfile className={styles.profileIcon} title='Profile Icon' /> 
-       <h1 className={styles.name}>User Name</h1>
-       <p className={styles.username}>@username</p>
+    <div className={styles.profileContainer}>
+        <Image 
+            src="/default-profile.png" 
+            alt={styles.imageBanner}
+            width={100} 
+            height={100} 
+            className={styles.imageBanner} />
+        <CgProfile className={styles.profileIcon} title='Profile Icon' /> 
+        <h1 className={styles.name}>User Name</h1>
+        <p className={styles.username}>@username</p>
     </div>
     
   );
