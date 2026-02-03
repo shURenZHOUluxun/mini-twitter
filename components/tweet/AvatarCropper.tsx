e9c5a99ff534e197a68229a32a0efdda9fdbb11a
Fabe Z <zengjiayifabe@outlook.com>
2026-02-02 23:38:20 -0500

    Update AvatarCropper.tsx

--- components/tweet/AvatarCropper.tsx
+++ components/tweet/AvatarCropper.tsx
@@ -9,3 +9,12 @@ type Area = {
     height: number;
 };
 
+async function createImage(url: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = url;
+  });
+}
